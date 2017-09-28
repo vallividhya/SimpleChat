6cf6d1362a0639b67643ec52546960b473130142
Valli Vidhya Venkatesan <vallividhyavenkatesan@gmail.com>
2017-09-27 20:03:20 -0700

    Recycler view layout

---
+++ app/src/main/java/com/vvv/simplechat/ChatApplication.java
@@ -0,0 +1,28 @@
+package com.vvv.simplechat;
+
+import com.parse.Parse;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+/**
+ * Created by vidhya on 9/27/17.
+ */
+
+public class ChatApplication extends android.app.Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("simplechat-client") // should correspond to APP_ID env variable
+                .clientBuilder(builder)
+                .server("https://codepath-chat-lab.herokuapp.com/parse/").build());
+    }
+}
