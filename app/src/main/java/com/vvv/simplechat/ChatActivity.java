5a66ccc18538141441dc8d185087614ccf90df20
Valli Vidhya Venkatesan <vallividhyavenkatesan@gmail.com>
2017-09-27 21:07:59 -0700

    Adapter, wiring up everything

--- app/src/main/java/com/vvv/simplechat/ChatActivity.java
+++ app/src/main/java/com/vvv/simplechat/ChatActivity.java
@@ -2,25 +2,39 @@ package com.vvv.simplechat;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
 import com.parse.LogInCallback;
 import com.parse.ParseAnonymousUtils;
 import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatActivity extends AppCompatActivity {
 
     static final String USER_ID_KEY = "userId";
     static final String BODY_KEY = "body";
+    static final int MAX_CHAT_MESSAGES_TO_SHOW = 50;
 
+    RecyclerView rvChat;
+    ChatAdapter mAdapter;
+    ArrayList<Message> mMessages;
     EditText etMessage;
     Button btSend;
+    // Keep track of initial load to scroll to the bottom of the ListView
+    boolean mFirstLoad;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +53,19 @@ public class ChatActivity extends AppCompatActivity {
         /// Find the text field and button
         etMessage = (EditText) findViewById(R.id.etMessage);
         btSend = (Button) findViewById(R.id.btSend);
+        rvChat = (RecyclerView) findViewById(R.id.rvChat);
+        mMessages = new ArrayList<>();
+        mFirstLoad = true;
+
+        final String userId = ParseUser.getCurrentUser().getObjectId();
+        mAdapter = new ChatAdapter(mMessages, userId, ChatActivity.this);
+        rvChat.setAdapter(mAdapter);
+
+        // associate the LayoutManager with the RecylcerView
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(ChatActivity.this);
+        linearLayoutManager.setReverseLayout(true);
+        rvChat.setLayoutManager(linearLayoutManager);
+
         btSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,6 +82,7 @@ public class ChatActivity extends AppCompatActivity {
                         if (e == null) {
                             Toast.makeText(ChatActivity.this, "Successfully created message on Parse",
                                     Toast.LENGTH_SHORT).show();
+                            refreshMessages();
 
                         } else {
                             Log.e("DEBUG", "Failed to save message", e);
@@ -65,6 +93,35 @@ public class ChatActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    // Query messages from Parse so we can load them into the chat adapter
+    void refreshMessages() {
+        // Construct query to execute
+        ParseQuery<Message> query = ParseQuery.getQuery(Message.class);
+        // Configure limit and sort order
+        query.setLimit(MAX_CHAT_MESSAGES_TO_SHOW);
+
+        // get the latest 50 messages, order will show up newest to oldest of this group
+        query.orderByDescending("createdAt");
+        // Execute query to fetch all messages from Parse asynchronously
+        // This is equivalent to a SELECT query with SQL
+        query.findInBackground(new FindCallback<Message>() {
+            public void done(List<Message> messages, ParseException e) {
+                if (e == null) {
+                    mMessages.clear();
+                    mMessages.addAll(messages);
+                    mAdapter.notifyDataSetChanged(); // update adapter
+                    // Scroll to the bottom of the list on initial load
+                    if (mFirstLoad) {
+                        rvChat.scrollToPosition(0);
+                        mFirstLoad = false;
+                    }
+                } else {
+                    Log.e("message", "Error Loading Messages" + e);
+                }
+            }
+        });
     }
 
     // Create an anonymous user using ParseAnonymousUtils and set sUserId
---
+++ app/src/main/java/com/vvv/simplechat/ChatAdapter.java
@@ -0,0 +1,96 @@
+package com.vvv.simplechat;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.util.List;
+
+/**
+ * Created by vidhya on 9/27/17.
+ */
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
+
+    List<Message> mMessages;
+    String userId;
+    Context context;
+
+    public ChatAdapter(List<Message> messageList, String userId, Context context) {
+        this.mMessages = messageList;
+        this.userId = userId;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View contactView = inflater.inflate(R.layout.item_chat, parent, false);
+
+        ViewHolder viewHodler = new ViewHolder(contactView);
+        return viewHodler;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Message message = mMessages.get(position);
+        final boolean isMe = message.getUserId() != null && message.getUserId().equals(userId);
+
+        if (isMe) {
+            holder.imageMe.setVisibility(View.VISIBLE);
+            holder.imageOther.setVisibility(View.GONE);
+            holder.body.setGravity(Gravity.CENTER_VERTICAL | Gravity.RIGHT);
+        } else {
+            holder.imageOther.setVisibility(View.VISIBLE);
+            holder.imageMe.setVisibility(View.GONE);
+            holder.body.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);
+        }
+
+        final ImageView profileView = isMe ? holder.imageMe : holder.imageOther;
+        Glide.with(context).load(getProfileUrl(message.getUserId())).into(profileView);
+        holder.body.setText(message.getBody());
+
+    }
+
+    private static String getProfileUrl(String userId) {
+        String hex = "";
+        try {
+            final MessageDigest digest = MessageDigest.getInstance("MD5");
+            final byte[] hash = digest.digest(userId.getBytes());
+            final BigInteger bigInt = new BigInteger(hash);
+            hex = bigInt.abs().toString(16);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "http://www.gravatar.com/avatar/" + hex + "?d=identicon";
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMessages.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageMe, imageOther;
+        TextView body;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            imageOther = (ImageView)itemView.findViewById(R.id.ivProfileOther);
+            imageMe = (ImageView)itemView.findViewById(R.id.ivProfileMe);
+            body = (TextView)itemView.findViewById(R.id.tvBody);
+
+        }
+    }
+}
